509a407cf732284705ee9066e25938be2cfd48fb
NoelVV <98539769+NoelVV@users.noreply.github.com>
2022-02-16 11:00:22 +0100

    Ultima Actualización CV

---
+++ CurriculumVitae_NVV.js
@@ -0,0 +1,6 @@
+function mostrarDatosOcultos() {
+    let datosOcultos = document.getElementById("datosOcultos");
+    if(datosOcultos.style.display = "none") {
+        datosOcultos.style.display = "block";
+    };
+};
